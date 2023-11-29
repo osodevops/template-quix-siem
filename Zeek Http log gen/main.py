8571123227c51dc213b21d454bc3b5e5356965c8
Matt Mcewan <matt@oso.sh>
2023-11-29 11:46:30 +0000

    Edited 'main.py'

--- Zeek Http log gen/main.py
+++ Zeek Http log gen/main.py
@@ -1,111 +1,81 @@
 import os
+import json
+import itertools
+import sys
 
+import random
+import string
 import time
 import quixstreams as qx
 import logging
-import random
 from dotenv import load_dotenv
-from datetime import datetime
-
 from quixstreams import RawMessage
 
 logging.basicConfig(level=logging.INFO)
 
 
-class AuthLogGen:
+class DnsLogGen:
     def __init__(self):
         is_container = bool(os.getenv('KUBERNETES_SERVICE_HOST'))
         load_dotenv()
         token = os.getenv("STREAMING_TOKEN")
-        logging.info("Preparing Quix Environment for Authentication logs gen")
+        logging.info("Preparing Quix Environment for DNS log gen")
         self.client = qx.QuixStreamingClient() if is_container else qx.QuixStreamingClient(token)
-        self.producer_topic = self.client.get_raw_topic_producer(os.environ.get("Topic",
-                                                                                "authentication-linux-logs"))
-        self.records_per_second = int(os.environ.get("RECS_PER_SECOND", 5))
-        self.users = [
-            "john_hacker_doe", "jane_goody2shoes_doe", "alice_adventure", "bob_builder",
-            "charlie_chaplin", "david_dreamer", "eve_explorer", "frank_finder",
-            "grace_gamer", "harry_hacker", "irene_inventor", "jack_jumper",
-            "steve_strider", "tina_traveler", "uma_user", "victor_visitor",
-            "wanda_watcher", "xavier_explorer", "yvonne_yielder", "zachary_zealous",
-            "larry_legend", "megan_mover", "nathan_navigator", "olivia_overseer",
-            "paula_pioneer", "quentin_quester", "rachel_racer", "sam_seeker",
-            "tracy_trekker", "ursula_undertaker", "vince_voyager", "wilma_wanderer"
-        ]
+        self.producer_topic = self.client.get_raw_topic_producer(os.environ.get("Topic", "zeek-http-logs"))
+        self.records_per_second = int(os.environ.get("DNS_PER_SECOND", 100))
 
-        self.actions = [
-            "login", "logout", "upload", "download", "update", "delete",
-            "create_account", "reset_password", "change_settings"
-        ]
+        base_path = os.path.dirname(__file__)
+        file_path = os.path.join(base_path, 'zeek-http.txt')
 
-        self.statuses = ["success", "failed", "pending", "error"]
-        self.device_types = ["mobile", "desktop", "laptop", "tablet", "server", "smartwatch"]
-        self.ports = ["80", "443", "22", "8080", "21", "25", "110", "143", "993", "995"]
+        self.dns_logs = []
 
-    def random_log_entry(self):
-        # Randomly select values from the lists
-        user = random.choice(self.users)
-        action = random.choice(self.actions)
-        status = random.choice(self.statuses)
-        device_type = random.choice(self.device_types)
-        port = random.choice(self.ports)
+        try:
+            with open(file_path, 'r') as file:
+                for line in file:
+                    # Convert each line from JSON to a Python dictionary
+                    try:
+                        json_log_entry = json.loads(line.strip())
+                        self.dns_logs.append(json_log_entry)
+                    except json.JSONDecodeError:
+                        logging.error(f"Invalid JSON format: {line.strip()} - skipping line")
+        except FileNotFoundError:
+            logging.error(f"File not found: {file_path}")
+            sys.exit()
 
-        # Generate a random IP address
-        src_ip = (f"{random.randint(50, 255)}.{random.randint(1, 255)}"
-                  f".{random.randint(1, 255)}.{random.randint(1, 255)}")
-
-        # Generate a silly gateway login to be caught by sigma rule
-        if random.randint(1, 150) == 50:
-            src_ip = "192.168.0.1"
-            action = "login"
-            logging.info("***************** SHOULD FIRE ALERT (Gateway) ********************")
-
-        if random.randint(1, 150) == 50:
-            action = "login"
-            port = "22"
-            logging.info("***************** SHOULD FIRE ALERT (Port 22) ********************")
-
-        if random.randint(1, 200) == 50:
-            action = "login"
-            src_ip = "192.168.0.1"
-            port = "22"
-            logging.info("***************** SHOULD FIRE ALERT (both) ********************")
-
-        # Generate a random timestamp for the current day
-        now = datetime.now()
-        timestamp = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        # Additional fields based on the action
-        extra_fields = ""
-        if action == "login" and status == "failed":
-            extra_fields = f', "login_attempts": {random.randint(1, 5)}'
-
-        # Create the log entry
-        log_entry = (f'{{"timestamp": "{timestamp}", "src_ip": "{src_ip}", "user": "{user}", "action": '
-                     f'"{action}", "status": "{status}", "device_type": "{device_type}", "port": '
-                     f'"{port}"{extra_fields}}}')
-
-        return log_entry, src_ip
-
-    def generate(self):
+    def yield_loop(self):
         interval = 1 / self.records_per_second
-        while True:
 
-            iteration_start_time = time.time()
-            log_entry, src_ip = self.random_log_entry()
-            print(log_entry)
-            key = bytearray(bytes(src_ip, 'utf-8'))
-            message = bytearray(bytes(log_entry, 'utf-8'))
-            message = RawMessage(message)
-            message.key = key
-            self.producer_topic.publish(message)
+        # Endlessly iterate over the dns_logs list
+        for curr_log_entry in itertools.cycle(self.dns_logs):
+            start_time = time.time()
+            random_id = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+            curr_log_entry['uid'] = random_id
+            curr_log_entry['ts'] = time.time()
+            yield curr_log_entry
 
             # Calculate elapsed time and sleep if necessary
-            elapsed_time = time.time() - iteration_start_time
+            elapsed_time = time.time() - start_time
             if elapsed_time < interval:
                 time.sleep(interval - elapsed_time)
-                self.producer_topic.flush()
+
+    def generate(self):
+        if self.dns_logs:
+            log_generator = self.yield_loop()
+            for i, log_entry in enumerate(log_generator):
+                json_string = json.dumps(log_entry)
+                key = bytearray(bytes(log_entry["id.orig_h"], 'utf-8'))
+                message = bytearray(bytes(json_string, 'utf-8'))
+                print(message)
+                message = RawMessage(message)
+                message.key = key
+
+                self.producer_topic.publish(message)
+                if i % self.records_per_second == 0:
+                    logging.info("DNS Recs generated: " + str(i))
+        else:
+            logging.info("No data found or failed to load data.")
 
 
 if __name__ == "__main__":
-    AuthLogGen().generate()
+    DnsLogGen().generate()
+
