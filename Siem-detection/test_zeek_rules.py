b54f937e04a88def3af6385d37335ba70fef74c6
Matt <35928174+StackAppsFinland@users.noreply.github.com>
2023-12-01 10:38:54 +0200

    Various tests and corrections to imported sigma rules.

--- Siem-detection/test_zeek_rules.py
+++ Siem-detection/test_zeek_rules.py
@@ -64,7 +64,102 @@ def test_not_condition():
     rule_id = add_rule("zeek_corelight_possible_webshell_-_dirty_word_list.yml")
 
     assert GoDetectorInterface().find_match_str(
+        malicious_log_data) == rule_id
+
+    assert GoDetectorInterface().find_match_str(
         friendly_log_data) == str()
+
+def test_greater_than():
+    # orig_bytes less than 1GB
+    # resp_bytes more than 100MB
+    malicious_log_data = """{
+  "id.orig_h": "192.168.1.10",
+  "id.orig_p": 12345,
+  "id.resp_h": "192.168.1.20",
+  "id.resp_p": 80,
+  "orig_bytes": 999999999,
+  "resp_bytes": 100000001,
+  "service": "http",
+  "duration": 15.5,
+  "orig_pkts": 9000,
+  "orig_ip_bytes": 920000000,
+  "resp_pkts": 8500,
+  "resp_ip_bytes": 160000000
+}"""
+
+    # orig_bytes less than 1GB
+    # resp_bytes less than 100MB
+    friendly_log_data = """
+{
+  "id.orig_h": "192.168.1.30",
+  "id.orig_p": 54321,
+  "id.resp_h": "192.168.1.40",
+  "id.resp_p": 443,
+  "orig_bytes": 1000000001,  
+  "resp_bytes": 99999999,
+  "service": "https",
+  "duration": 10.2,
+  "orig_pkts": 5000,
+  "orig_ip_bytes": 510000000,
+  "resp_pkts": 4000,
+  "resp_ip_bytes": 55000000
+}
+"""
+
+    rule_id = add_rule("zeek_corelight_client_sending_large_amount_of_data.yml")
 
     assert GoDetectorInterface().find_match_str(
         malicious_log_data) == rule_id
+
+    assert GoDetectorInterface().find_match_str(
+        friendly_log_data) == str()
+
+
+def test_regex():
+    # Non-ASCII characters in URI, fulfilling the selection criteria
+    malicious_log_data = """{
+  "ts": "2023-12-01T12:00:00.000000Z",
+  "uid": "C1LrSi3BKY30IjwHja",
+  "id.orig_h": "10.0.0.1",
+  "id.orig_p": 50500,
+  "id.resp_h": "192.168.1.15",
+  "id.resp_p": 80,
+  "trans_depth": 1,
+  "method": "POST",
+  "host": "example.com",
+  "uri": "/upload/файл",  
+  "version": "1.1",
+  "user_agent": "Mozilla/5.0",
+  "request_body_len": 1024,
+  "response_body_len": 200,
+  "status_code": 200,
+  "status_msg": "OK"
+}"""
+
+    # Same log with a valid uri
+    friendly_log_data = """{
+  "ts": "2023-12-01T12:00:00.000000Z",
+  "uid": "C1LrSi3BKY30IjwHja",
+  "id.orig_h": "10.0.0.1",
+  "id.orig_p": 50500,
+  "id.resp_h": "192.168.1.15",
+  "id.resp_p": 80,
+  "trans_depth": 1,
+  "method": "POST",
+  "host": "example.com",
+  "uri": "/upload/options?fileName=text.txt", 
+  "version": "1.1",
+  "user_agent": "Mozilla/5.0",
+  "request_body_len": 1024,
+  "response_body_len": 200,
+  "status_code": 200,
+  "status_msg": "OK"
+}"""
+
+    rule_id = add_rule("zeek_corelight_http_post_or_put_uri_non_ascii_character.yml")
+
+    assert GoDetectorInterface().find_match_str(
+        malicious_log_data) == rule_id
+
+    assert GoDetectorInterface().find_match_str(
+        friendly_log_data) == str()
